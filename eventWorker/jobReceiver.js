3a972bd82cacee8bbd113579a2168f0ff30262fb
Kishore <kishoreh@guidanz.com>
2020-03-13 14:41:16 +0530

    path changed

--- eventWorker/jobReceiver.js
+++ eventWorker/jobReceiver.js
@@ -1,3 +1,3 @@
 const Queue=require('bull');
 const receiverQueue=new Queue('pdfReport');
-receiverQueue.process(3,'/home/guidanz-kishore/Documents/workspace/skedler-new_Kish-new/eventWorker/workerProcess')
+receiverQueue.process(3,'/home/guidanz-kishore/Documents/skedler-new_Kish-new/eventWorker/workerProcess')
